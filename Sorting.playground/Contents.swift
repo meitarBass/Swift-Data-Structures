10cc17d3b11a5498e9d1f4008a8eeed09420d2d5
meitarbasson <meitarbass@gmail.com>
2024-10-23 23:38:35 +0300

    Insertion Sort

--- Sorting.playground/Contents.swift
+++ Sorting.playground/Contents.swift
@@ -1,3 +1,4 @@
+// O(n^2)
 public func bubbleSort<Element>(_ array: inout [Element]) where Element: Comparable {
     guard array.count >= 2 else { return }
     for end in (1 ..< array.count).reversed() {
@@ -12,6 +13,7 @@ public func bubbleSort<Element>(_ array: inout [Element]) where Element: Compara
     }
 }
 
+// O(n^2)
 public func selectionSort<Element>(_ array: inout [Element]) where Element: Comparable {
     guard array.count >= 2 else { return }
     for current in 0 ..< (array.count - 1) {
@@ -23,5 +25,20 @@ public func selectionSort<Element>(_ array: inout [Element]) where Element: Comp
         }
         
         if lowest != current { array.swapAt(lowest, current) }
+    }
+}
+
+
+// O(n^2)
+public func insertionSort<Element>(_ array: inout [Element]) where Element: Comparable {
+    guard array.count >= 2 else { return }
+    for current in 1 ..< array.count {
+        for shifting in (1...current).reversed() {
+            if array[shifting] < array[shifting - 1] {
+                array.swapAt(shifting, shifting - 1)
+            } else {
+                break
+            }
+        }
     }
 }
