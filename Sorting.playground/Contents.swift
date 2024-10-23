8f0e98af6be99513213ffd810e729efbdee5da8c
meitarbasson <meitarbass@gmail.com>
2024-10-23 23:33:23 +0300

    Selection sort

--- Sorting.playground/Contents.swift
+++ Sorting.playground/Contents.swift
@@ -11,3 +11,17 @@ public func bubbleSort<Element>(_ array: inout [Element]) where Element: Compara
         if !swapped { return }
     }
 }
+
+public func selectionSort<Element>(_ array: inout [Element]) where Element: Comparable {
+    guard array.count >= 2 else { return }
+    for current in 0 ..< (array.count - 1) {
+        var lowest = current
+        for other in (current + 1) ..< array.count {
+            if array[lowest] > array[other] {
+                lowest = other
+            }
+        }
+        
+        if lowest != current { array.swapAt(lowest, current) }
+    }
+}
