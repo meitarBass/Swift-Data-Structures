b7bd3b4b354ebce04b298b2aa846e3f6a120daf6
meitarbasson <meitarbass@gmail.com>
2024-10-24 10:45:42 +0300

    Added merge sort

--- Sorting.playground/Contents.swift
+++ Sorting.playground/Contents.swift
@@ -42,3 +42,51 @@ public func insertionSort<Element>(_ array: inout [Element]) where Element: Comp
         }
     }
 }
+
+//  O(n) - we have total of n elements (left + right)
+// when sorting we have n*O(1) operations (choose correct element to insert now, n times)
+private func merge<Element>(_ left: [Element], _ right: [Element]) -> [Element] where Element: Comparable {
+    var leftIndex = 0
+    var rightIndex = 0
+    
+    var result: [Element] = []
+    while leftIndex < left.count && rightIndex < right.count {
+        let leftElement = left[leftIndex]
+        let rightElement = right[rightIndex]
+        
+        if leftElement < rightElement {
+            result.append(leftElement)
+            leftIndex += 1
+        } else if leftElement > rightElement {
+            result.append(rightElement)
+            rightIndex += 1
+        } else {
+            result.append(leftElement)
+            leftIndex += 1
+            result.append(rightElement)
+            rightIndex += 1
+        }
+    }
+    
+    if leftIndex < left.count {
+        result.append(contentsOf: left[leftIndex...])
+    }
+    
+    if rightIndex < right.count {
+        result.append(contentsOf: right[rightIndex...])
+    }
+    
+    return result
+}
+
+// O(nlog(n)) time and space complexity
+public func mergeSort<Element>(_ array: [Element]) -> [Element] where Element: Comparable {
+    guard array.count > 1 else { return array }
+    
+    let middle = array.count / 2
+    
+    let left = mergeSort(Array(array[..<middle]))
+    let right = mergeSort(Array(array[middle...]))
+    
+    return merge(left, right)
+}
