98cd3f2942791b1617be64f4e1d663d73114e0f0
meitarbasson <meitarbass@gmail.com>
2024-10-26 12:41:10 +0300

    Revert "Adjacency matrix"
    This reverts commit 52dc12362019d642b3fff26c440c94e5d4e4ecec.

--- Graphs.playground/Contents.swift
+++ Graphs.playground/Contents.swift
@@ -96,113 +96,17 @@ extension AdjacencyList: CustomStringConvertible {
   
   public var description: String {
     var result = ""
-    for (vertex, edges) in adjacencies {
+    for (vertex, edges) in adjacencies { // 1
       var edgeString = ""
-      for (index, edge) in edges.enumerated() {
+      for (index, edge) in edges.enumerated() { // 2
         if index != edges.count - 1 {
           edgeString.append("\(edge.destination), ")
         } else {
           edgeString.append("\(edge.destination)")
         }
       }
-      result.append("\(vertex) ---> [ \(edgeString) ]\n")
+      result.append("\(vertex) ---> [ \(edgeString) ]\n") // 3
     }
     return result
   }
-}
-
-public class AdjacencyMatrix<T>: Graph {
-    
-    private var vertices: [Vertex<T>] = []
-    private var weights: [[Double?]] = []
-    
-    public init() {}
-    
-    public func createVertex(data: T) -> Vertex<T> {
-        let vertex = Vertex(index: vertices.count, data: data)
-        vertices.append(vertex)
-        for i in 0..<weights.count {
-            // column (index) is 0 - still no edges to this vertex
-            weights[i].append(nil)
-        }
-        
-        // new row - still no edges from this vertex
-        let row = [Double?](repeating: nil, count: vertices.count)
-        weights.append(row)
-        return vertex
-    }
-    
-    public func addDirectedEdge(from source: Vertex<T>, to destination: Vertex<T>, weight: Double?) {
-        weights[source.index][destination.index] = weight
-    }
-    
-    public func edges(from source: Vertex<T>) -> [Edge<T>] {
-        var edges: [Edge<T>] = []
-        for column in 0..<weights.count {
-            if let weight = weights[source.index][column] {
-                edges.append(Edge(source: source, destination: vertices[column], weight: weight))
-            }
-        }
-        return edges
-    }
-    
-    public func weight(from source: Vertex<T>, to destination: Vertex<T>) -> Double? {
-        weights[source.index][destination.index]
-    }
-}
-
-extension AdjacencyMatrix: CustomStringConvertible {
-  
-  public var description: String {
-    let verticesDescription = vertices.map { "\($0)" }
-                                      .joined(separator: "\n")
-    var grid: [String] = []
-    for i in 0..<weights.count {
-      var row = ""
-      for j in 0..<weights.count {
-        if let value = weights[i][j] {
-          row += "\(value)\t"
-        } else {
-          row += "ø\t\t"
-        }
-      }
-      grid.append(row)
-    }
-    let edgesDescription = grid.joined(separator: "\n")
-    
-    return "\(verticesDescription)\n\n\(edgesDescription)"
-  }
-}
-
-
-let graph = AdjacencyMatrix<String>()
-
-let singapore = graph.createVertex(data: "Singapore")
-let tokyo = graph.createVertex(data: "Tokyo")
-let hongKong = graph.createVertex(data: "Hong Kong")
-let detroit = graph.createVertex(data: "Detroit")
-let sanFrancisco = graph.createVertex(data: "San Francisco")
-let washingtonDC = graph.createVertex(data: "Washington DC")
-let austinTexas = graph.createVertex(data: "Austin Texas")
-let seattle = graph.createVertex(data: "Seattle")
-
-graph.add(.undirected, from: singapore, to: hongKong, weight: 300)
-graph.add(.undirected, from: singapore, to: tokyo, weight: 500)
-graph.add(.undirected, from: hongKong, to: tokyo, weight: 250)
-graph.add(.undirected, from: tokyo, to: detroit, weight: 450)
-graph.add(.undirected, from: tokyo, to: washingtonDC, weight: 300)
-graph.add(.undirected, from: hongKong, to: sanFrancisco, weight: 600)
-graph.add(.undirected, from: detroit, to: austinTexas, weight: 50)
-graph.add(.undirected, from: austinTexas, to: washingtonDC, weight: 292)
-graph.add(.undirected, from: sanFrancisco, to: washingtonDC, weight: 337)
-graph.add(.undirected, from: washingtonDC, to: seattle, weight: 277)
-graph.add(.undirected, from: sanFrancisco, to: seattle, weight: 218)
-graph.add(.undirected, from: austinTexas, to: sanFrancisco, weight: 297)
-
-print(graph)
-
-print("San Francisco Outgoing Flights:")
-print("--------------------------------")
-for edge in graph.edges(from: sanFrancisco) {
-  print("from: \(edge.source) to: \(edge.destination)")
 }
