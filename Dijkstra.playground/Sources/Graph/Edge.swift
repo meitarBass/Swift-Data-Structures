10489966a58657b6e99c2a44a28fb7c0bbff2ab8
meitarbasson <meitarbass@gmail.com>
2024-10-27 15:04:55 +0200

    Dijkstra algorithm

---
+++ Dijkstra.playground/Sources/Graph/Edge.swift
@@ -0,0 +1,9 @@
+public struct Edge<T> {
+  
+  public let source: Vertex<T>
+  public let destination: Vertex<T>
+  public let weight: Double?
+}
+
+extension Edge: Hashable where T: Hashable {}
+extension Edge: Equatable where T: Equatable {}
