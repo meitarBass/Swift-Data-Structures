ebba7c7fc9c8b8eaaa84ab85c346405d66c75591
meitarbasson <meitarbass@gmail.com>
2024-10-28 08:19:56 +0200

    Added the required files

---
+++ Prim.playground/Sources/Graph/Edge.swift
@@ -0,0 +1,8 @@
+public struct Edge<T> {
+  
+  public let source: Vertex<T>
+  public let destination: Vertex<T>
+  public let weight: Double?
+}
+
+extension Edge: Equatable where T: Equatable {}
---
+++ Prim.playground/Sources/Graph/Prim.swift
@@ -0,0 +1,7 @@
+//
+//  Prim.swift
+//  
+//
+//  Created by Meitar Basson on 28/10/2024.
+//
+
